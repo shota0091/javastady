36f38f5f5164243ea2ad30b49daef3a99c518c1e
shoya0091 <syou0091@yahoo.co.jp>
2021-05-23 20:35:02 +0900

    テストコミット

---
+++ SampleProject/src/jp/learnlingdesign/javastudy/DisplayMonth2.java
@@ -0,0 +1,13 @@
+package jp.learnlingdesign.javastudy;
+
+public class DisplayMonth2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String[] month = {"Jaunary","Feburay","March","April","May","June","July","August","September","October","Novenber","Decenber"};
+		for(int i = 0; i < 12; i++) {
+			System.out.println(month[i]);
+		}
+	}
+
+}
